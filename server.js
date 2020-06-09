83b616a326bd53a089173c5eecd62a26540c9a8b
Krishna Chaitanya B V <krishcbv@gmail.com>
2020-06-09 11:02:50 +0530

    Hopeful change

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
 
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)});
 
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res,db)});
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)});
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`Running on port ${process.env.PORT}`)
